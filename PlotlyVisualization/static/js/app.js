fb31644d6895cf057b2acffba1418f603d503d14
Jagrati Joshi <jagrati.joshi@outlook.com>
2020-12-30 23:02:19 -0700

    Updated event error

--- PlotlyVisualization/static/js/app.js
+++ PlotlyVisualization/static/js/app.js
@@ -5,6 +5,9 @@ function init() {
     // Assign the value of the dropdown menu option to a variable
     //var selectedValue = d3.select("#selDataset option:checked").property("value");
     barPlot(null);
+    loadDemographics(null);
+    loadGauge(null);
+    loadBubble(null);
 }
 
 function loadIDs() {
@@ -22,18 +25,22 @@ function loadIDs() {
 function optionChanged(selectedValue) {
     console.log(selectedValue);
     barPlot(selectedValue);
+    loadDemographics(selectedValue);
+    loadGauge(selectedValue);
+    loadBubble(selectedValue);
 }
 // Use d3.json() to fetch data from JSON file
 // Incoming data is internally referred to as importedData
 function barPlot(selectedID) {
+    if (selectedID == null) {
+        selectedID = d3.select("#selDataset option:checked").property("value");
+    }
     d3.json("data/samples.json").then((importedData) => {
+        
         var data = importedData.samples;
 
-        if (selectedID == null) {
-            selectedID = d3.select("#selDataset option:checked").property("value");
-        }
         console.log(selectedID);
-        
+
         var filteredData = data.filter(row => row.id === selectedID);
         console.log(filteredData);
 
@@ -42,7 +49,7 @@ function barPlot(selectedID) {
         filteredData.sort(function(a, b) {
             return parseFloat(b.sample_values) - parseFloat(a.sample_values);
         });
-
+        filteredData[0].sample_values.sort((sample1, sample2) => sample1 - sample2);
         // Slice the first 10 objects for plotting
         filteredData = filteredData.slice(0, 10);
 
@@ -52,7 +59,7 @@ function barPlot(selectedID) {
         // Create your trace.
         var trace = {
             x: filteredData[0].sample_values.slice(0,10).reverse(),
-            y: filteredData[0].otu_ids.slice(0,10).reverse(),
+            //y: filteredData[0].otu_ids.slice(0,10).reverse().map(row => row),
             text: filteredData[0].otu_labels.slice(0,10).reverse(),
             name: "OTU",
             type: "bar",
@@ -78,8 +85,31 @@ function barPlot(selectedID) {
     });
 }
 
-function filterData(data,id) {
-    return data.id = id;
+function loadDemographics(selectedID) {
+    if (selectedID == null) {
+        selectedID = d3.select("#selDataset option:checked").property("value");
+    }
+    var demographicsDiv = d3.select("#sample-metadata");
+    d3.json("data/samples.json").then((importedData) => {
+        var data = importedData.metadata;
+        var filteredData = data.filter(row => row.id === selectedID);
+        console.log(filteredData);
+        demographicsDiv.text(filteredData);
+    });
+}
+
+function loadGauge(selectedID) {
+    if (selectedID == null) {
+        selectedID = d3.select("#selDataset option:checked").property("value");
+    }
+    var gaugeDiv = d3.select("#gauge");
+}
+
+function loadBubble(selectedID) {
+    if (selectedID == null) {
+        selectedID = d3.select("#selDataset option:checked").property("value");
+    }
+    var bubbleDiv = d3.select("#bubble");
 }
 
 init();
